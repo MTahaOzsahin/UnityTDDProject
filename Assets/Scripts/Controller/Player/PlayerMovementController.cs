a1988e40525611ed0c00fdbbda55f4cd5cec7766
MTahaOzsahin <87945619+MTahaOzsahin@users.noreply.github.com>
2023-11-27 17:53:49 +0300

    New input system WIP

--- Assets/Scripts/Controller/Player/PlayerMovementController.cs
+++ Assets/Scripts/Controller/Player/PlayerMovementController.cs
@@ -1,5 +1,6 @@
-using System;
+using InputSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Controller.Player
 {
@@ -8,13 +9,57 @@ namespace Controller.Player
         [Header("Components")]
         private CharacterController playerCharacterController;
 
-        [Header("Variables")]
-        [SerializeField] private float playerMovementSpeed; 
-        private float inputPressedThresholdInSecond;
+        [Header("Variables")] 
+        private bool isPlayerHolding;
+        private float playerHoldingTimeInSeconds;
+        private Vector3 playerDirection;
+
+        [Header("Inputs")] 
+        private TDDProtectInputMaster playerInputMaster;
+        
 
         private void Awake()
         {
             playerCharacterController = GetComponent<CharacterController>();
+            playerInputMaster = new TDDProtectInputMaster();
+        }
+
+        private void OnEnable()
+        {
+            playerInputMaster.Enable();
+            playerInputMaster.PlayerController.Move.started += StartMove;
+            playerInputMaster.PlayerController.Run.performed += StartRun;
+            playerInputMaster.PlayerController.Run.canceled += StopMovement;
+        }
+
+        private void OnDisable()
+        {
+            playerInputMaster.PlayerController.Run.performed -= StartRun;
+            playerInputMaster.PlayerController.Move.started -= StartMove;
+            playerInputMaster.PlayerController.Run.canceled -= StopMovement;
+            playerInputMaster.Disable();
+        }
+
+        private void StartMove(InputAction.CallbackContext value)
+        {
+            var originDirection = value.ReadValue<Vector2>();
+            playerDirection = new Vector3(originDirection.x, 0f, originDirection.y);
+            Debug.Log("Move");
+        }
+
+        private void StartRun(InputAction.CallbackContext value)
+        {
+            var originDirection = value.ReadValue<Vector2>();
+            playerDirection = new Vector3(originDirection.x, 0f, originDirection.y);
+            playerHoldingTimeInSeconds = (float)value.duration;
+            isPlayerHolding = true;
+            Debug.Log("Run" + playerHoldingTimeInSeconds);
+        }
+
+        private void StopMovement(InputAction.CallbackContext value)
+        {
+            isPlayerHolding = false;
+            playerDirection = Vector3.zero;
         }
 
         private void Update()
@@ -24,30 +69,10 @@ namespace Controller.Player
 
         private void PlayerMovement()
         {
-            if (Input.GetKey(KeyCode.W))
+            playerCharacterController.Move(playerDirection * (Time.deltaTime));
+            if (isPlayerHolding)
             {
-                playerCharacterController.Move(Vector3.forward * (Time.deltaTime * playerMovementSpeed * inputPressedThresholdInSecond));
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                playerCharacterController.Move(-Vector3.right * (Time.deltaTime * playerMovementSpeed * inputPressedThresholdInSecond));
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                playerCharacterController.Move(-Vector3.forward * (Time.deltaTime * playerMovementSpeed * inputPressedThresholdInSecond));
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                playerCharacterController.Move(Vector3.right * (Time.deltaTime * playerMovementSpeed * inputPressedThresholdInSecond));
-            }
-        }
-
-        private void HandleInputThreshold()
-        {
-            inputPressedThresholdInSecond = 0f;
-            if (Input.GetKey(KeyCode.A))
-            {
-                
+                playerCharacterController.Move(playerDirection * (Time.deltaTime * playerHoldingTimeInSeconds) );
             }
         }
     }
